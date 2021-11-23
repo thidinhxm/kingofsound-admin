a9e722092f723aa9a7ced6fd4a3da5cdf9299c4a
thidinhxm <thidinhxm@gmail.com>
2021-11-23 11:41:54 +0700

    format code

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('/products/products');
+  res.render('products/products');
 });
 
 router.get('/add-product', (req, res, next) => {
