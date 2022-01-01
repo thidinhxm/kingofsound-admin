cb22d24794d9d5bc616abdb9545e7f928c67a702
Hoang Truong <hoangtruongvann@gmail.com>
2022-01-01 08:37:44 +0700

    add try catch

--- components/dashboard/dashboardController.js
+++ components/dashboard/dashboardController.js
@@ -1,8 +1,13 @@
 const orderService = require('../orders/orderService');
 
 exports.index = async (req, res, next) => {
+	try{
 	const active = {dashboard: true};
 	const listTop10 = await orderService.getTop10();
 
 	res.render('../components/dashboard/dashboardViews/index', {listTop10,active});
+}
+catch (err) {
+	next(err);
+}
 }
--- components/products/productController.js
+++ components/products/productController.js
@@ -15,47 +15,62 @@ cloudinary.config({
 
 const itemPerPage = 8;
 exports.list = async (req, res) => {
-	const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
-	const search_name = req.query.search_name;
-	let categories = await productService.listcategory();
-	const active = { product: true }
+	try {
+		const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
+		const search_name = req.query.search_name;
+		let categories = await productService.listcategory();
+		const active = { product: true }
 
-	if (search_name) {
-		const products = await productService.listByName(search_name, page);
-		res.render('../components/products/productViews/products', {
-			products: products.rows,
-			categories,
-			search_name,
-			Pages: products.count / itemPerPage,
-			active
-		});
+		if (search_name) {
+			const products = await productService.listByName(search_name, page);
+			res.render('../components/products/productViews/products', {
+				products: products.rows,
+				categories,
+				search_name,
+				Pages: products.count / itemPerPage,
+				active
+			});
+		}
+		else {
+			const products = await productService.list(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+			res.render('../components/products/productViews/products', {
+				products: products.rows,
+				categories
+				, Pages: products.count / itemPerPage,
+				active
+			});
+		}
 	}
-	else {
-		const products = await productService.list(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
-		res.render('../components/products/productViews/products', {
-			products: products.rows,
-			categories
-			, Pages: products.count / itemPerPage,
-			active
-		});
+	catch (err) {
+		next(err);
 	}
 }
 
 
 exports.listByName = async (req, res) => {
-	const search_name = req.query.search_name;
-	const products = await productService.listByName(search_name, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
-	const categories = await productService.listcategory();
-	const active = { product: true }
+	try {
+		const search_name = req.query.search_name;
+		const products = await productService.listByName(search_name, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+		const categories = await productService.listcategory();
+		const active = { product: true }
 
-	res.render('../components/products/productViews/products', { products, categories, active });
+		res.render('../components/products/productViews/products', { products, categories, active });
+	}
+	catch (err) {
+		next(err);
+	}
 }
 
 exports.add = async (req, res) => {
-	const categories = await productService.listcategory();
-	const active = { product: true }
+	try {
+		const categories = await productService.listcategory();
+		const active = { product: true }
 
-	res.render('../components/products/productViews/add-product', { categories, active });
+		res.render('../components/products/productViews/add-product', { categories, active });
+	}
+	catch (err) {
+		next(err);
+	}
 
 }
 
@@ -71,106 +86,125 @@ const path = require('path');
 exports.store = async (req, res, next) => {
 
 
+	try {
+		// const selectedCategory = await models.categories.findOne({where: {category_name: req.body.category}, raw: true})
 
-	// const selectedCategory = await models.categories.findOne({where: {category_name: req.body.category}, raw: true})
+		// const newProduct = await models.products.create({
+		// 	category_id: selectedCategory.category_id,
+		// 	product_name: req.body.name,
+		// 	price: req.body.price,
+		// 	descriptions: req.body.descriptions,
+		// 	model_year: req.body.model_year,
+		// 	is_active: 1,
+		// });
 
-	// const newProduct = await models.products.create({
-	// 	category_id: selectedCategory.category_id,
-	// 	product_name: req.body.name,
-	// 	price: req.body.price,
-	// 	descriptions: req.body.descriptions,
-	// 	model_year: req.body.model_year,
-	// 	is_active: 1,
-	// });
+		// const imgProduct = await models.images.create({
+		// 	product_id: newProduct.product_id,
+		// 	image_stt: 1,
+		// 	image_link: req.body.image_link
+		// });
 
-	// const imgProduct = await models.images.create({
-	// 	product_id: newProduct.product_id,
-	// 	image_stt: 1,
-	// 	image_link: req.body.image_link
-	// });
-
-	// res.redirect('/products');
-	// res.json(newProduct);
-	// res.json(req.body);
-	// console.log(req.file);
-
-
+		// res.redirect('/products');
+		// res.json(newProduct);
+		// res.json(req.body);
+		// console.log(req.file);
 
 
 
-	// const linkImage = async function generatePublicUrl() {
-	// 	try {
-	// 		const fileId = imageID+'';
-	// 		await drive.permissions.create({
-	// 			fileId: fileId,
-	// 			requestBody: {
-	// 				role: 'reader',
-	// 				type: 'anyone',
-	// 			},
-	// 		});
-
-	// 		/*
-	// 		webViewLink: View the file in browser
-	// 		webContentLink: Direct download link
-	// 		*/
-	// 		const result = await drive.files.get({
-	// 			fileId: fileId,
-	// 			fields: 'webViewLink, webContentLink',
-	// 		});
-	// 		console.log(result.data);
-	// 	} catch (error) {
-	// 		console.log(error.message);
-	// 	}
 
 
-	// }`
-	const listLinkImages = await productService.getListLinkImage();
-	res.json(listLinkImages)
+		// const linkImage = async function generatePublicUrl() {
+		// 	try {
+		// 		const fileId = imageID+'';
+		// 		await drive.permissions.create({
+		// 			fileId: fileId,
+		// 			requestBody: {
+		// 				role: 'reader',
+		// 				type: 'anyone',
+		// 			},
+		// 		});
+
+		// 		/*
+		// 		webViewLink: View the file in browser
+		// 		webContentLink: Direct download link
+		// 		*/
+		// 		const result = await drive.files.get({
+		// 			fileId: fileId,
+		// 			fields: 'webViewLink, webContentLink',
+		// 		});
+		// 		console.log(result.data);
+		// 	} catch (error) {
+		// 		console.log(error.message);
+		// 	}
 
 
-	// linkImage();
+		// }`
+		const listLinkImages = await productService.getListLinkImage();
+		res.json(listLinkImages)
+
+
+		// linkImage();
+	}
+	catch (err) {
+		next(err);
+	}
 }
 
 exports.edit = async (req, res) => {
-	const currentProduct = await models.products.findOne({ where: { product_id: req.params.id }, raw: true })
-	const currentCategory = currentProduct.category_id;
-	const imgProduct = await models.images.findOne({ where: { product_id: req.params.id }, raw: true });
-	const curentCategoryProduct = await models.categories.findOne({ where: { category_id: currentCategory }, raw: true });
-	const categories = await productService.listcategory();
-	const active = { product: true }
+	try {
+		const currentProduct = await models.products.findOne({ where: { product_id: req.params.id }, raw: true })
+		const currentCategory = currentProduct.category_id;
+		const imgProduct = await models.images.findOne({ where: { product_id: req.params.id }, raw: true });
+		const curentCategoryProduct = await models.categories.findOne({ where: { category_id: currentCategory }, raw: true });
+		const categories = await productService.listcategory();
+		const active = { product: true }
 
 
-	res.render('../components/products/productViews/edit-product', { currentProduct, categories, curentCategoryProduct, imgProduct, active });
-	// res.json(curentCategoryProduct)
+		res.render('../components/products/productViews/edit-product', { currentProduct, categories, curentCategoryProduct, imgProduct, active });
+		// res.json(curentCategoryProduct)
+	}
+	catch (err) {
+		next(err);
+	}
 }
 
 exports.update = async (req, res, next) => {
-	const categoryUpade = await models.categories.findOne({ where: { category_name: req.body.category, } })
-	const productUpdate = {
-		product_name: req.body.name,
-		price: req.body.price,
-		category_id: categoryUpade.category_id,
-		model_year: req.body.model_year,
-		descriptions: req.body.descriptions
-	}
+	try {
+		const categoryUpade = await models.categories.findOne({ where: { category_name: req.body.category, } })
+		const productUpdate = {
+			product_name: req.body.name,
+			price: req.body.price,
+			category_id: categoryUpade.category_id,
+			model_year: req.body.model_year,
+			descriptions: req.body.descriptions
+		}
 
-	await models.products.update(productUpdate, { where: { product_id: req.params.id } })
-		(res.redirect('/products'))
-	// res.json(categoryUpade)
+		await models.products.update(productUpdate, { where: { product_id: req.params.id } })
+			(res.redirect('/products'))
+		// res.json(categoryUpade)
+	}
+	catch (err) {
+		next(err);
+	}
 }
 
 
 
 exports.delete = async (req, res) => {
-	await models.products.update({
-		is_active: false
-	}, {
-		where: {
-			product_id: req.params.id,
-		}
-	})
-		// const currentProduct = await models.products.findOne({ where: { product_id: req.params.id }, raw: true })
+	try {
+		await models.products.update({
+			is_active: false
+		}, {
+			where: {
+				product_id: req.params.id,
+			}
+		})
+			// const currentProduct = await models.products.findOne({ where: { product_id: req.params.id }, raw: true })
 
-		(res.redirect('/products'));
+			(res.redirect('/products'));
+	}
+	catch (err) {
+		next(err);
+	}
 };
 
